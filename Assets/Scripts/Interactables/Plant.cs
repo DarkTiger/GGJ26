3aae58ef07d396a1fc4d8740ac5a94e5040136be
BalbiCreativeLab <159083570+BalbiCreativeLab@users.noreply.github.com>
2026-02-01 00:37:56 +0100

    started recipe book ui, plant to ingredient, machine prefabs

--- Assets/Scripts/Interactables/Plant.cs
+++ Assets/Scripts/Interactables/Plant.cs
@@ -4,6 +4,7 @@ public class Plant : Item
 {
     public bool IsWet { get; set; }
     public int GrowLevel { get; private set; } = 0;
+    public GameObject IngredientPrefab;
 
     float growingTime;
     const float GROW_TIME = 30f;
@@ -35,5 +36,18 @@ public class Plant : Item
         {
             GetComponent<Collider2D>().enabled = true;
         }
+    }
+
+    public override void OnInteract(Player player)
+    {
+        if(player.CurrentItem != null)
+            return;
+
+        GameObject ingredient = Instantiate(IngredientPrefab);
+        ingredient.transform.position = transform.position;
+        
+        ingredient.GetComponent<Interactable>().OnInteract(player);
+
+        Destroy(gameObject);
     }
 }
