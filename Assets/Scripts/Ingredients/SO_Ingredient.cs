964d0ed5898ee2b37ce383bb33f3b1a27a76dc3f
BalbiCreativeLab <159083570+BalbiCreativeLab@users.noreply.github.com>
2026-01-31 14:01:49 +0100

    Update SO_Ingredient.cs

--- Assets/Scripts/Ingredients/SO_Ingredient.cs
+++ Assets/Scripts/Ingredients/SO_Ingredient.cs
@@ -5,5 +5,7 @@ public class SO_Ingredient : ScriptableObject
 {
     public ItemStatus startStatus = ItemStatus.BASE;
     [Space(10)]
-    public Sprite baseSprite, cookedSprite, powderSprite;
+    public Sprite baseSprite;
+    public Sprite cookedSprite;
+    public Sprite powderSprite;
 }
