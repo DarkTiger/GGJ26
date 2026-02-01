717a71502ee066dd624e646325e5a8ea15459996
Lor3nz14 <60523526+Lor3nz14@users.noreply.github.com>
2026-02-01 11:26:56 +0100

    Update bed prefab and add comin cost

--- Assets/Scripts/Interactables/Bed.cs
+++ Assets/Scripts/Interactables/Bed.cs
@@ -5,24 +5,46 @@ public class Bed : Interactable
 {
     [SerializeField] Sprite freeSprite, busy;
     [SerializeField] Transform checkpointBed;
+    [SerializeField] GameObject comicCost;
     public int CostUnlock;
     public bool IsFree = true;
     public bool IsOpen = false;
+    private bool comicShow = false;
 
     public override void OnInteract(Player player)
     {
         base.OnInteract(player);
-        if (!IsOpen) //TODO DA AGGIUNGERE IL CONTROLLO SE SI HANNO SOLDI
+        if (!IsOpen) 
         {
             if (CostUnlock<= GameManager.Instance.GetMoney())
             {
                 GameManager.Instance.DecreaseMoney(CostUnlock);
                 GetComponent<SpriteRenderer>().sprite = freeSprite;
+                comicCost.SetActive(false);
                 IsFree = true;
                 IsOpen = true;
             }
         }
     }
+
+    public override void OnEntering(Player player)
+    {
+        base.OnEntering(player);
+
+        if (!IsOpen)
+        {
+            comicShow = true;
+            comicCost.SetActive(true);
+        }
+    }
+
+    public override void OnExit(Player player)
+    {
+        base.OnExit(player);
+        comicShow = false;
+        comicCost.SetActive(false);
+    }
+
 
     public void SetBedBusyState()
     {
